65cea61db09fcd8ee7fbc7e683dd9ed07be77907
Eric Chen <deeplearningericchen@gmail.com>
2023-07-26 13:47:35 -0700

    starting to write whatsapp tool

--- twitter-whatsapp-agent/tools/whatsapp.py
+++ twitter-whatsapp-agent/tools/whatsapp.py
@@ -1,5 +1,15 @@
+import pywhatkit
+
 from .tool import Tool
 
 
 class WhatsAppTool(Tool):
-    pass
+    name = "WhatsApp"
+    description = "Use to send a WhatsApp message to myself."
+    input_schema = "(message: str)"
+    output_schema = "Sent!"
+
+    def __call__(self, message: str):
+        pywhatkit
+
+        return "Sent!"
